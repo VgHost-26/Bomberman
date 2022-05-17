f405571c5ccfedd86cf8da0af1971105d3f94e46
Martin <97972556+VgHost-26@users.noreply.github.com>
2022-05-17 18:53:27 +0200

    New

---
+++ Bomberman/menu.cpp
@@ -0,0 +1,22 @@
+/* plik z menu gry */
+
+using namespace std;
+//biblioteki systemowe
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <conio.h>
+#include <ctime>
+
+//biblioteki swoje
+#include "functions_system.h"
+#include "functions_menu.h"
+#include "globals.h"
+
+int main(){
+
+
+    system("start game.exe");
+
+    return 0;
+}
