912e9c19ef0a8c175dff69131bac6e215455b906
wybielak <31769799+wybielak@users.noreply.github.com>
2022-05-21 14:08:35 +0200

    Wyświetlanie logo w menu głównym
    Logo mniej więcej wyświetla się w menu. Martin nie złość się, że pożyczyłem twoje funkcje, ale działają tak dobrze, że nie opłaca się pisać nowych :D

--- GameFiles/functions_menu.h
+++ GameFiles/functions_menu.h
@@ -1,4 +1,23 @@
 //funkcje wykorzystywane w pliku menu.cpp
 #pragma once
 
+void printLogo(int color, int place){   //wyswietlanie logo na środku ekranu   
+    
+    fstream plik;
+    string line{};
+    plik.open("logo.txt");
+    SetConsoleTextAttribute(hConsole, color);
+    cout<<endl<<endl;
+    for(int i=0;i<13;i++){
+        cout.width(place);
+        
+        getline(plik,line);
+        cout<<line<<endl;
+    }
+    cout<<endl<<endl;
+    plik.close();  
+}
 
+int middleCalc(int winWidth, int objWidth){     //obliczanie miejsca na środku ekranu dla konkretnego elementu wzgledem szerokości ekranu
+    return winWidth-((winWidth-objWidth)/2);
+}
--- GameFiles/menu.cpp
+++ GameFiles/menu.cpp
@@ -6,8 +6,12 @@
 #include <windows.h>
 #include <conio.h>
 #include <ctime>
+#include <fstream>
+#include <string>
 
 using namespace std;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 //biblioteki swoje
 #include "functions_system.h"
@@ -16,7 +20,20 @@ using namespace std;
 
 int main(){
 
-    cout<<"Boomboom"<<endl;
+    setConsoleSize();
+    hideCursor();
+
+    //pobieranie wielkości konsoli (centrowanie)
+    CONSOLE_SCREEN_BUFFER_INFO csbi;    
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    int winWidth = csbi.dwMaximumWindowSize.X;
+    int winHeight = csbi.dwMaximumWindowSize.Y;
+
+    int defCol = 7; //Domyślny kolor tekstu;
+
+    printLogo(defCol, middleCalc(winWidth, 119));
+
+    cout.width(middleCalc(winWidth, 7)); cout<<"Boomboom"<<endl;
     system("pause");
     system("start game.exe");
 
