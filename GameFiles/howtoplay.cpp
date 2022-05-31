ec71825da452ee0b3705071391cc5686503d205d
Gesiek <gesiek200@gmail.com>
2022-05-31 15:14:43 +0200

    hotoplay

---
+++ GameFiles/howtoplay.cpp
@@ -0,0 +1,26 @@
+/* plik z pomoca do gry */
+
+//biblioteki systemowe
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include <conio.h>
+#include <ctime>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+//biblioteki swoje
+#include "globals.h"
+#include "functions_system.h"
+#include "functions_menu.h"
+
+int main(){
+
+    setConsoleSize();
+    hideCursor();
+    setColors();
+
+    cout<<"TEST"<<endl;
+}
